cac52826543b1641046f82bcb6da42482ecad059
Fazer56 <faoilean56@gmail.com>
2018-04-18 10:32:12 +0200

    Commit

---
+++ Library/Controllers/CustomerController.cs
@@ -0,0 +1,63 @@
+﻿using Library.Models;
+using System.Diagnostics;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+	public class CustomerController : Controller
+	{
+		// GET: Customer
+		public ActionResult Login()
+		{
+			/* To Display a list of fields in database
+			LibraryEntities db = new LibraryEntities();
+			return View(db.Customers.ToList());*/
+			return View();
+		}
+
+		// GET: Customer
+		/*The Index() method of the Home controller is the default method for an ASP.NET MVC application. 
+		  When you run an ASP.NET MVC application, the Index() method is the first controller method that
+		  is called.*/
+
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Login(Customer objUser)
+		{
+			//used to validate that the rules of the model are being applied. ie required fields and correct format 
+			if (ModelState.IsValid)
+			{
+				
+				using (LibraryEntities db = new LibraryEntities())
+				{
+					var obj = db.Customers.Where(a => a.CustEmail.Equals(objUser.CustEmail) && a.CPassword.Equals(objUser.CPassword)).FirstOrDefault();
+					
+					if (obj != null)
+					{
+						Session["custID"] = obj.CustID.ToString();
+						Session["custName"] = obj.CustName.ToString();
+						Session["custEmail"] = obj.CustEmail.ToString();
+						Debug.WriteLine(Session["custID"].ToString());
+						return RedirectToAction("UserArea");
+					}
+				}
+			}
+			return View(objUser);
+		}
+
+		[Route("customer/UserArea")]
+		public ActionResult UserArea()
+		{
+			if (Session["custID"] != null)
+			{
+				return View();
+			}
+			else
+			{
+				return RedirectToAction("Login");
+			}
+		}
+	}
+}
---
+++ Library/Controllers/LibrarianController.cs
@@ -0,0 +1,78 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+	public class LibrarianController : Controller
+	{
+		// GET: Librarian
+		public ActionResult StaffLogin()
+		{
+			return View();
+
+		}
+
+		//Tells runtime where to send form post data back to
+		[HttpPost]
+		//Protects against hackers
+		[ValidateAntiForgeryToken]
+		public ActionResult StaffLogin(Librarian libUser)
+		{
+			//Check that form data is in correct format for the model and obeys all the rules
+			if (ModelState.IsValid)
+			{
+				//Database object for accessing data
+				using (LibraryEntities db = new LibraryEntities())
+				{
+					//Check the password and ID match
+					var obj = db.Librarians.Where(a => a.LibrarianID.Equals(libUser.LibrarianID) && a.Password.Equals(libUser.Password)).FirstOrDefault();
+					Debug.WriteLine(obj.Name.ToString());
+					if (obj != null)
+					{
+						//Set the session variables with values from the database
+						Session["libID"] = obj.LibrarianID.ToString();
+						Session["libLocation"] = obj.LibLocation.ToString();
+						Session["libName"] = obj.Name.ToString();
+						Session["pos"] = obj.Position.ToString();
+						//redirect to the librarian page
+						return RedirectToAction("StaffArea");
+
+					}
+
+				}
+			}
+			return View(libUser);
+
+		}
+
+		public ActionResult StaffArea()
+		{
+			return View();
+			/*
+			if (Session["libID"] != null)
+			{
+
+				return View();
+			}
+			else
+			{
+				return RedirectToAction("StaffLogin");
+			} */
+		}
+
+		/*[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult StaffArea()
+		{
+
+
+			return View();
+
+		}*/
+	}
+}
