921059eca307125921a1794165843ec92c905ab4
eriky15 <eriky15@student.uia.no>
2018-04-20 18:19:19 +0100

    testing to push

---
+++ Library/Controllers/AboutController .cs
@@ -0,0 +1,20 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: Info
+        public ActionResult Info()
+        {
+			LibraryEntities db = new LibraryEntities();
+			
+            return View();
+        }
+    }
+}
---
+++ Library/Controllers/PricingController.cs
@@ -0,0 +1,20 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class PricingController : Controller
+    {
+        // GET: Pricing
+        public ActionResult PriceList()
+        {
+            LibraryEntities db = new LibraryEntities();
+
+            return View();
+        }
+    }
+}
